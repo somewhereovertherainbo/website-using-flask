28518eb82ac5638a06dc33d41cc75f95f4dd3f37
somewhereovertherainbo <yathindrasairahul000@gmail.com>
2024-03-07 14:47:34 +0530

    added html file

--- #CREATING WEBSITE.py
+++ #CREATING WEBSITE.py
@@ -1,11 +1,11 @@
 #CREATING WEBSITE
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return 'Whatever'
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)
